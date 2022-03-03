7f57ce11c72ad9f3bda8cf0e082186d0d0f4f634
emiliasroka <100830526+emiliasroka@users.noreply.github.com>
2022-03-03 08:25:22 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define pi 3.14
+
+int main(void) {
+int r = 6;
+  double pole = pi*(r*r);
+  printf("Pole jest równe: %f", pole); 
+  return 0;
+}
+/* dowiedzieć się na czym polega funkcja scanf (wprowadzanie danych do programu) 
+zrobić przynajmniej 3 pierwsze zadania 
+dokończyć aktywację konta na github*/
+
